4898ef752497b61b68e4499371ff119c4a9ba93e
Alan Tan <alan@Alans-MacBook-Pro-2.local>
2023-01-04 20:26:09 -0800

    added a spinning animation using keyframes

--- js.js
+++ js.js
@@ -1,10 +1,19 @@
+let globalVar = 0; 
+let player_winnings = 0; 
+let computer_winnings = 0; 
+let winner_display = 'VS';  
+
+const right_side_number = document.querySelector('div.right-side-number'); 
+const left_side_number = document.querySelector('div.left-side-number'); 
+const middle_content = document.querySelector('div.middle-content'); 
+
 function getComputerChoice() {
     randomInt = Math.floor(Math.random() * 3) + 1;
     if(randomInt == 1){
-        return "Rock";
+        return "Rock-button";
     }
     else if (randomInt == 2){
-        return "Scissors";
+        return "Scissor-button";
     }
     else{
         return "Paper"; 
@@ -40,20 +49,20 @@ function playRound(playerSelection){
     let computerNumber;
     let winner; 
 
-    if(playerSelection == "Rock"){
+    if(playerSelection == "Rock-button"){
         playerNumber = 1; 
     }
-    else if (playerSelection == "Scissor"){
+    else if (playerSelection == "Scissor-button"){
         playerNumber = 2; 
     }
     else{
         playerNumber = 3; 
     }
 
-    if(computerSelection == "Rock"){
+    if(computerSelection == "Rock-button"){
         computerNumber = 1; 
     }
-    else if(computerSelection == "Scissor"){
+    else if(computerSelection == "Scissor-button"){
         computerNumber = 2; 
     }
     else{
@@ -66,31 +75,65 @@ function playRound(playerSelection){
 
     if (winner == 1){
         console.log (`You win! ${playerSelection} beats ${computerSelection}!`)
-        return (`win`);  
+        globalVar = (`win`); 
     }
     else if (winner == 3){
         console.log (`It's a tie! ${computerSelection} ties with ${playerSelection}!`);
-        return (`tie`); 
+        globalVar = ('tie'); 
     }
     else {
         console.log (`You lose! ${computerSelection} beats ${playerSelection}!`);
-        return (`lose`); 
+        globalVar = ('lose'); 
+    }   
+
+    if (computer_winnings == 5){
+    winner_display = "COMPUTER WINS"
+    middle_content.innerHTML = `${winner_display}`
+    computer_winnings = 0; 
+    player_winnings = 0; 
+    right_side_number.innerHTML = `${computer_winnings}`
+    left_side_number.innerHTML = `${player_winnings}`
+    return; 
+    }
+    else if (player_winnings == 5){
+    winner_display = "PLAYER WINS"
+    middle_content.innerHTML = `${winner_display}`
+    computer_winnings = 0; 
+    player_winnings = 0; 
+    right_side_number.innerHTML = `${computer_winnings}`
+    left_side_number.innerHTML = `${player_winnings}`
+    return; 
+    }
+
+    if (globalVar == 'win') {
+        player_winnings++; 
+        right_side_number.innerHTML = `${player_winnings}`;
+        winner_display = `PLAYER POINT`; 
+        middle_content.innerHTML = `${winner_display}`;
+        return;
+    }
+    else if (globalVar == 'lose') {
+        computer_winnings++;
+        left_side_number.innerHTML = `${computer_winnings}`; 
+        winner_display = `COMPUTER POINT`; 
+        middle_content.innerHTML = `${winner_display}`; 
+        return;
+    }
+    else {
+        winner_display = `TIE`; 
+        middle_content.innerHTML = `${winner_display}`; 
+        return;
     }
 }
 
+
+// This is when the program actually starts
 const rpsButtons = document.querySelectorAll('div.buttons button');
 rpsButtons.forEach((button) => {
-    button.addEventListener('click', () => playRound(button.className));
+button.addEventListener('click', () => playRound(button.className));
 });
 
-let player_winnings = 0; 
-let computer_winnings = 0; 
-let winner_display; 
 
-const right_side_number = document.querySelector('right-side-number'); 
-const left_side_number = document.querySelector('left-side-number'); 
-const middle_content = document.querySelector('middle-content'); 
 
-for (let i = 0; i < 5; i++){
-    if 
-}
+
+
